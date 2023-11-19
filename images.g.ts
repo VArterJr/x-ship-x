14ec8ee8312f543206352f22858104359c55f610
Vince Arter, Jr <vince@arter.us>
2023-11-19 16:13:19 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "pcShip":return img`
+. . . . . . . . . . . . . . . . 
+. f f . . . . . . . . . . . . . 
+. . f f f . . . . . . . . . . . 
+. . f 1 1 f f . . . . . . . . . 
+. . f 1 1 1 f f f . . . . . . . 
+. . f 1 1 1 f 1 1 f f f . . . . 
+. . . f 1 1 f 1 1 1 1 f f f . . 
+. . . 2 f f f f f f f 5 5 f f . 
+. . . f 1 1 f 1 1 1 1 f f f . . 
+. . f 1 1 1 f 1 1 f f f . . . . 
+. . f 1 1 1 f f f . . . . . . . 
+. . f 1 1 f f . . . . . . . . . 
+. . f f f . . . . . . . . . . . 
+. f f . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "npcShipBlob":return img`
+. . . . . . . . . . . f . . . . 
+. . . . . . . . . . f f . . . . 
+. . . . . . . . . f 4 f . . . . 
+. f f f f f f f f 4 4 f . . . . 
+f e e e e e e e f 4 4 f . . . . 
+f e e e e e e e f 4 4 f . . . . 
+f e e e e e e e f 4 4 f . . . . 
+f e e e e e e e f 4 4 f . . . . 
+f e e e e e e e f 4 4 f . . . . 
+f e e e e e e e f 4 4 f . . . . 
+f e e e e e e e f 4 4 f . . . . 
+. f f f f f f f f 4 4 f . . . . 
+. . . . . . . . . f 4 f . . . . 
+. . . . . . . . . . f f . . . . 
+. . . . . . . . . . . f . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
