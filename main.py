e7783838859a3adb9e259f788723c9826c620b9e
Vince Arter, Jr <vince@arter.us>
2023-11-19 16:49:22 -0600

    Fixed issues with multiplayer positioning.

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ pcShipPlayer1 = sprites.create(assets.image("""
 pcShipPlayer2 = sprites.create(assets.image("""
     npcShipBlob
 """), SpriteKind.player)
-pcShipPlayer1.set_position(0, scene.screen_height() / 2 + 5)
-pcShipPlayer2.set_position(scene.screen_width() - 5, scene.screen_height() / 2 + 5)
+pcShipPlayer1.set_position(5, scene.screen_height() / 2)
+pcShipPlayer2.set_position(scene.screen_width() - 5, scene.screen_height() / 2)
 
 def on_forever():
-    controller.player1.move_sprite(pcShipPlayer2)
+    controller.player1.move_sprite(pcShipPlayer1)
     controller.player2.move_sprite(pcShipPlayer2)
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let pcShipPlayer1 = sprites.create(assets.image`pcShip`, SpriteKind.Player)
 let pcShipPlayer2 = sprites.create(assets.image`npcShipBlob`, SpriteKind.Player)
-pcShipPlayer1.setPosition(5, scene.screenHeight() / 2 + 5)
-pcShipPlayer2.setPosition(scene.screenWidth() - 5, scene.screenHeight() / 2 + 5)
+pcShipPlayer1.setPosition(5, scene.screenHeight() / 2)
+pcShipPlayer2.setPosition(scene.screenWidth() - 5, scene.screenHeight() / 2)
 forever(function () {
     controller.player1.moveSprite(pcShipPlayer1)
     controller.player2.moveSprite(pcShipPlayer2)
